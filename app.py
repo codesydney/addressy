da9fe46d46c632af52bfc42a644102ee9cce72ab
Engramar Bollas <engramar.bollas@gmail.com>
2023-07-09 09:20:40 +1000

    Handling of Ajax in Flask

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import os
-from flask import Flask, render_template, flash, redirect, url_for, Markup, request
+from flask import Flask, render_template, flash, redirect, url_for, Markup, request, jsonify
 app = Flask(__name__)
 app.loqate_user = "\"" + os.getenv('LOQATE_USER') + "\"" 
 app.loqate_password = "\""  + os.getenv('LOQATE_PASSWORD') + "\"" 
-
 
 @app.route('/')
 def index():
     return render_template('index.html',user=app.loqate_user,password=app.loqate_password)
 
-@app.route('/show_insights',methods=['POST'])
-def show_insights():
-  print ("In show_insights")      
- 
+@app.route('/home',methods=['POST'])
+def home():
+    print ("In home")
+    return render_template('index.html',user=app.loqate_user,password=app.loqate_password)
+
+@app.route('/process/<chosen_address>')
+def process(chosen_address):
+    print (type(chosen_address))
+    print (chosen_address)
+    return render_template("result.html", chosen_address = chosen_address)
+
 # 404 error handler
 @app.errorhandler(404)
 def page_not_found(e):
